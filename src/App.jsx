f199e5a062d1dd4313494eeb3ad2023d22385252
tatiana <taatt2000@gmail.com>
2025-01-28 12:02:16 +0100

    28/01

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,94 @@
+import { useState } from 'react';
+import { PRODUCTS } from './constants/products';
+
 const App = () => {
-	return <h1>React</h1>;
+	const [sortBy, setSortBy] = useState(0);
+	const [cart, setCart] = useState(0)
+
+	const sortedProducts = sortProducts(PRODUCTS, sortBy);
+	const cartProducts = 
+
+	return (
+		<>
+			<header>
+				<h1>Desserts</h1>
+				<div>
+					<button onClick={() => setSortBy(0)}>Default</button>
+					<button onClick={() => setSortBy(1)}>Name</button>
+					<button onClick={() => setSortBy(2)}>Price</button>
+				</div>
+			</header>
+
+			<main>
+				<section>
+					{sortedProducts.map(product => (
+						<article key={product.id}>
+							<picture>
+								<source
+									media='(min-width: 1024px)'
+									srcSet={product.imgDesktop}
+								/>
+								<source media='(min-width: 768px)' srcSet={product.imgTablet} />
+								<source media='(min-width: 320px)' srcSet={product.imgMobile} />
+								<img src={product.imgMobile} alt='' />
+							</picture>
+							<button onClick={() => addToCart(product, cart, setCart)}>Add to cart</button>
+							<div>
+								<button>
+									<img
+										src='/assets/images/icon-decrement-quantity.svg'
+										alt=''
+									/>
+								</button>
+								<span>1</span>
+								<button>
+									<img
+										src='/assets/images/icon-increment-quantity.svg'
+										alt=''
+									/>
+								</button>
+							</div>
+							<div>
+								<span>{product.name}</span>
+								<h2>{product.title}</h2>
+								<span>{product.price}</span>
+							</div>
+						</article>
+					))}
+				</section>
+
+				<section>
+					<h3>Your cart</h3>
+					<img src='/assets/images/illustration-empty-cart.svg' alt='' />
+					<div>
+
+					</div>
+
+					<div>
+						<span>Total order</span>
+						<span>$46.50</span>
+					</div>
+
+					<button>Confirm order</button>
+				</section>
+			</main>
+		</>
+	);
 };
+
+const sortProducts = (products, sortBy) => {
+	// Lógica para ordenar
+	const copyProducts = [...products];
+	if (sortBy === 0) return copyProducts;
+	if (sortBy === 1)
+		return copyProducts.sort((a, b) => a.name.localeCompare(b.name));
+	if (sortBy === 2) return copyProducts.sort((a, b) => a.price - b.price);
+	//devuelves productos ordenados
+};
+
+const addToCart = (product, cart, setCart) => {
+	setCart([...cart, product])
+
+}
+
 export default App;
---
+++ src/constants/products.js
@@ -0,0 +1,109 @@
+export const PRODUCTS = [
+	{
+		id: '9d6b6f24-948c-4b62-b48d-3c1f6b7a69df',
+		name: 'Waffle',
+		title: 'Waffle with Berries',
+		price: 6.5,
+		imgMobile: '/assets/images/image-waffle-mobile.jpg',
+		imgTablet: '/assets/images/image-waffle-tablet.jpg',
+		imgDesktop: '/assets/images/image-waffle-desktop.jpg',
+		imgThumbnail: '/assets/images/image-waffle-thumbnail.jpg',
+		alt: 'A waffle tower with sliced strawberries and icing sugar.'
+	},
+
+	{
+		id: '2f85c6b9-b16b-4f9d-b6ab-1d7e6e347635',
+		name: 'Crème Brûlée',
+		title: 'Vanilla Bean Crème Brûlée',
+		price: 7.0,
+		imgMobile: '/assets/images/image-creme-brulee-mobile.jpg',
+		imgTablet: '/assets/images/image-creme-brulee-tablet.jpg',
+		imgDesktop: '/assets/images/image-creme-brulee-desktop.jpg',
+		imgThumbnail: '/assets/images/image-creme-brulee-thumbnail.jpg',
+		alt: 'Two dishes of crème brûlée topped with berries, figs and mint.'
+	},
+
+	{
+		id: '457e0b42-4b6a-4d91-a3f0-9d7b6f1e29e9',
+		name: 'Macaron',
+		title: 'Macaron Mix of Five',
+		price: 8.0,
+		imgMobile: '/assets/images/image-macaron-mobile.jpg',
+		imgTablet: '/assets/images/image-macaron-tablet.jpg',
+		imgDesktop: '/assets/images/image-macaron-desktop.jpg',
+		imgThumbnail: '/assets/images/image-macaron-thumbnail.jpg',
+		alt: 'Five different flavour macarons.'
+	},
+
+	{
+		id: '3f906f5b-f4f7-4414-8a9c-7a8b52f8a2c4',
+		name: 'Tiramisu',
+		title: 'Classic Tiramisu',
+		price: 5.5,
+		imgMobile: '/assets/images/image-tiramisu-mobile.jpg',
+		imgTablet: '/assets/images/image-tiramisu-tablet.jpg',
+		imgDesktop: '/assets/images/image-tiramisu-desktop.jpg',
+		imgThumbnail: '/assets/images/image-tiramisu-thumbnail.jpg',
+		alt: 'A tiramisu topped with berries, mint and cocoa powder.'
+	},
+
+	{
+		id: 'b8e91464-4b6f-4dc7-8f92-bd524f2d67df',
+		name: 'Baklava',
+		title: 'Pistachio Baklava',
+		price: 4.0,
+		imgMobile: '/assets/images/image-baklava-mobile.jpg',
+		imgTablet: '/assets/images/image-baklava-tablet.jpg',
+		imgDesktop: '/assets/images/image-baklava-desktop.jpg',
+		imgThumbnail: '/assets/images/image-baklava-thumbnail.jpg',
+		alt: 'Three triangle-shaped baklavas topped with crushed pistachios.'
+	},
+
+	{
+		id: 'c0b87e38-a687-451e-9bb6-4c8d9b9adfa9',
+		name: 'Pie',
+		title: 'Lemon Meringue Pie',
+		price: 5.0,
+		imgMobile: '/assets/images/image-meringue-mobile.jpg',
+		imgTablet: '/assets/images/image-meringue-tablet.jpg',
+		imgDesktop: '/assets/images/image-meringue-desktop.jpg',
+		imgThumbnail: '/assets/images/image-meringue-thumbnail.jpg',
+		alt: 'A slice of lemon pie coronated with meringue.'
+	},
+
+	{
+		id: '3d16f842-7d5f-4fb8-88a2-9b69b5a7f8b7',
+		name: 'Cake',
+		title: 'Red Velvet Cake',
+		price: 4.5,
+		imgMobile: '/assets/images/image-cake-mobile.jpg',
+		imgTablet: '/assets/images/image-cake-tablet.jpg',
+		imgDesktop: '/assets/images/image-cake-desktop.jpg',
+		imgThumbnail: '/assets/images/image-cake-thumbnail.jpg',
+		alt: 'Red velvet cake portion with sliced strawberries on the inside and a berry on top.'
+	},
+
+	{
+		id: 'f69d15b8-a947-4b9f-8e27-f65d98b6d5a9',
+		name: 'Brownie',
+		title: 'Salted Caramel Brownie',
+		price: 5.5,
+		imgMobile: '/assets/images/image-brownie-mobile.jpg',
+		imgTablet: '/assets/images/image-brownie-tablet.jpg',
+		imgDesktop: '/assets/images/image-brownie-desktop.jpg',
+		imgThumbnail: '/assets/images/image-brownie-thumbnail.jpg',
+		alt: 'A portion of brownie with vanilla ice cream and caramel syrup.'
+	},
+
+	{
+		id: 'b29f68d3-a8c4-4b7e-9d37-f5a7e18c7d4f',
+		name: 'Cotta',
+		title: 'Vanilla Panna Cotta',
+		price: 6.5,
+		imgMobile: '/assets/images/image-panna-cotta-mobile.jpg',
+		imgTablet: '/assets/images/image-panna-cotta-tablet.jpg',
+		imgDesktop: '/assets/images/image-panna-cotta-desktop.jpg',
+		imgThumbnail: '/assets/images/image-panna-cotta-thumbnail.jpg',
+		alt: 'Two crystal jars of panna cotta with cream.'
+	}
+];
